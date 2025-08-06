a816468e5cd1471aa1eefc3e30ff2efa8fbc9f55
Fabio Vallejo <fabiovallejot@gmail.com>
2025-08-06 13:41:03 -0500

    Pequeña modificacion img

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar({ onTheme, darkTheme }) {
         <nav className={` ${darkTheme === true ? "bg-[#1F2535] text-white" : "bg-[#FCFDFF]"} mb-9 md:mb-20 p-3 md:p-5 flex rounded-[25px] justify-between items-center`}>
             <Logo className="w-35 md:w-50" />
             <button className={`${darkTheme === true ? "bg-[#2F354B] focus:bg-[#525868] focus:outline-offset-2 focus:border-[#040A1C] focus:outline-[#D4686C] focus:outline-2 hover:bg-[#525868]" : "bg-[#EEEEEE] focus:outline-offset-2 focus:border-[#ECF4FB] focus:outline-[#CA241B] focus:outline-2 hover:bg-[#C6C6C5]" } duration-200 transition-all p-4 rounded-[15px] cursor-pointer`} onClick={onTheme}>
-                <img src={` ${darkTheme === true ? "../../public/images/sun.svg" : "../../public/images/icon-moon.svg" }`} alt="" />
+                <img src={` ${darkTheme === true ? "/images/sun.svg" : "/images/icon-moon.svg" }`} alt="" />
             </button>
         </nav>
     );
